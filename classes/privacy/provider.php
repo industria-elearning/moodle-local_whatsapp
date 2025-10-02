81538fd45f45ec970f2f038eced68fb37caee80f
wilber-buendata <wilber@buendata.com>
2025-10-02 12:26:10 -0500

    remove unused files and updating license headers

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -1,22 +1,27 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace local_whatsapp\privacy;
 
 /**
- * Whatsapp Chat.
+ * Class provider
  *
  * @package    local_whatsapp
- * @copyright 2020 BuenData Developer <hernan@buendata.com>
- */
-
-
-namespace whatsapp\privacy;
-
-defined('MOODLE_INTERNAL') || die();
-
-/**
- * Privacy Subsystem for local_whatsapp implementing null_provider.
- *
- * @package    local_whatsapp
- * @copyright 2020 BuenData Developer <hernan@buendata.com>
+ * @copyright  2025 Wilber Narvaez <wilber@buendata.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class provider implements \core_privacy\local\metadata\null_provider {
 
